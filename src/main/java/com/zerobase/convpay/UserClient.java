714451cfc8b6cb0b80b60faeefc801a7c165e6cc
zune <changzunelee@gmail.com>
2022-06-06 02:35:03 +0900

    Initial commit

--- src/main/java/com/zerobase/convpay/UserClient.java
+++ src/main/java/com/zerobase/convpay/UserClient.java
@@ -1,15 +1,24 @@
 package com.zerobase.convpay;
 
+import com.zerobase.convpay.config.ApplicationConfig;
 import com.zerobase.convpay.dto.*;
 import com.zerobase.convpay.service.ConveniencePayService;
 import com.zerobase.convpay.type.PayMethodType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserClient {
     public static void main(String[] args) {
 
         //사용자 ->편의점결제 - 머니
 
-        ConveniencePayService conveniencePayService = new ConveniencePayService();
+        ApplicationContext applicationContext  = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+        ConveniencePayService conveniencePayService =
+                applicationContext.getBean("conveniencePayService",
+                        ConveniencePayService.class);
+
+
 
         //gs25에서  결제 1000원
         PayRequest payRequest = new PayRequest(PayMethodType.CARD, ConvenienceType.G25, 1000);
